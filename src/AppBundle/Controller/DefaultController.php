e4f17038c186be24bb15a9ba99ac243fc8e2295f
mathieu becerra <matheiu.becerra.beweb@gmail.com>
2017-02-16 15:21:56 +0100

    commit quais

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,6 +5,8 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
 class DefaultController extends Controller
 {
     /**
@@ -13,8 +15,31 @@ class DefaultController extends Controller
     public function indexAction(Request $request)
     {
         // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
-        ]);
+        return $this->render('default/couleurs/green.html.twig');
+    }
+    /**
+     * @Route("/admin", name="admin")
+     */
+    public function adminAction()
+    {
+        return new Response('<html><body>Admin page!</body></html>');
+    }
+    /**
+     * @Route("/logout", name="logout")
+     */
+    public function logout(){
+        
+    }
+    /**
+     * @Route("/login", name="login")
+     */
+    public function login(){
+        return $this->render('default/couleurs/login.html.twig');
+    }
+    /**
+     * @Route("/loginCheck", name="loginCheck")
+     */
+    public function loginCheck(){
+        
     }
 }
